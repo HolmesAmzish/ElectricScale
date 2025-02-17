d525c8367431219a404308645e54166e315dcc77
Holmes <holmesamzish86@outlook.com>
2025-02-17 23:17:05 +0800

    Test opencv with pytorch

---
+++ test/opencv_with_pytorch.py
@@ -0,0 +1,56 @@
+import cv2
+import torch
+from torch import nn
+from torchvision import transforms, models
+from PIL import Image
+import numpy as np
+
+model = torch.load("../models/model.pth", weights_only=False, map_location=torch.device('cpu'))
+model.eval()
+
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: Failed to capture image")
+        break
+
+    image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    image = transform(image).unsqueeze(0)
+
+    with torch.no_grad():
+        outputs = model(image)
+        _, predicted = torch.max(outputs, 1)
+
+    class_idx = predicted.item()
+    class_names = ['apple', 'banana', 'beetroot', 'bell pepper', 'cabbage', 'capsicum', 'carrot', 'cauliflower',
+                   'chilli pepper', 'corn', 'cucumber', 'eggplant', 'garlic', 'ginger', 'grapes', 'jalepeno', 'kiwi',
+                   'lemon', 'lettuce', 'mango', 'onion', 'orange', 'paprika', 'pear', 'peas', 'pineapple',
+                   'pomegranate', 'potato', 'raddish', 'soy beans', 'spinach', 'sweetcorn', 'sweetpotato', 'tomato',
+                   'turnip', 'watermelon']
+
+    predicted_class = class_names[class_idx]
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, f'Predicted: {predicted_class}', (10, 30), font, 1, (0, 255, 0), 2)
+
+    # 显示视频帧
+    cv2.imshow("Fruit Recognition", frame)
+
+    # 按 'q' 键退出
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# 释放视频捕捉对象和关闭窗口
+cap.release()
+cv2.destroyAllWindows()
+cv2.destroyAllWindows()
